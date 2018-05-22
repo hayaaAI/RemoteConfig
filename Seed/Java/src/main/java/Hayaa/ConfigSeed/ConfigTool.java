003c71b20ac6556ea12878a88f5dfdb15db00878
hayaa <dev@hayaa-ai.com>
2018-05-22 20:05:03 +0800

    添加java数据库配置

--- Seed/Java/src/main/java/Hayaa/ConfigSeed/ConfigTool.java
+++ Seed/Java/src/main/java/Hayaa/ConfigSeed/ConfigTool.java
@@ -1,57 +1,58 @@
 package Hayaa.ConfigSeed;
 
-import hayaa.basemodel.model.AppSettings;
-import hayaa.basemodel.model.ConfigContent;
-import hayaa.basemodel.model.ConnectionStrings;
-import hayaa.basemodel.model.FunctionOpenResult;
+import hayaa.basemodel.model.*;
 
 import java.util.Optional;
 
 public class ConfigTool<T extends ConfigContent, APPROOT> {
     private T g_config;
     private APPROOT g_appRootConfig;
-    public ConfigTool(int componentID,Class<T> componentConfigType,Class<APPROOT> appRootConfigType)
-    {
-        FunctionOpenResult<T> Config = AppSeed.GetConfig(componentID,componentConfigType);
-        if (Config.isActionResult())
-        {
+
+    public ConfigTool(int componentID, Class<T> componentConfigType, Class<APPROOT> appRootConfigType) {
+        FunctionOpenResult<T> Config = AppSeed.GetConfig(componentID, componentConfigType);
+        if (Config.isActionResult()) {
             g_config = Config.getData();
         }
         FunctionOpenResult<APPROOT> appRootConfig = AppSeed.GetAppRootConfig(appRootConfigType);
-        if (Config.isActionResult())
-        {
+        if (Config.isActionResult()) {
             g_appRootConfig = appRootConfig.getData();
         }
     }
-    public T GetComponentConfig()
-    {
+
+    public T GetComponentConfig() {
         return g_config;
     }
-    public APPROOT GetAppRootConfig()
-    {
+
+    public APPROOT GetAppRootConfig() {
         return g_appRootConfig;
     }
-    public String GetConnection(String name)
-    {
 
+    public MariadbConfig getDBConfig(String name) {
+        ConfigContent baseConfig = (ConfigContent) g_config;
+        Optional<MariadbConfig> con = baseConfig.getDbConfigs().stream()
+                .filter(c -> c.getKey().equals(name)).findFirst();
+        return con.get();
+    }
+
+    public String GetConnection(String name) {
         return GetCon(name);
     }
-    private String GetCon(String name)
-    {
-        ConfigContent baseConfig = (ConfigContent)g_config;
+
+    private String GetCon(String name) {
+        ConfigContent baseConfig = (ConfigContent) g_config;
         Optional<ConnectionStrings.ConnectionInfo> con = baseConfig.getConnectionStrings().Settings.stream()
-                .filter(c->c.getName().equals(name)).findFirst();
+                .filter(c -> c.getName().equals(name)).findFirst();
         return con.get().getConnection();
     }
-    public String GetAppsetting(String key, String defaultVal)
-    {
+
+    public String GetAppsetting(String key, String defaultVal) {
         return GetAppset(key, defaultVal);
     }
-    private String GetAppset(String key, String defaultVal)
-    {
-        ConfigContent baseConfig = (ConfigContent)g_config;
+
+    private String GetAppset(String key, String defaultVal) {
+        ConfigContent baseConfig = (ConfigContent) g_config;
         Optional<AppSettings.AppSetInfo> con = baseConfig.getAppSettings().Settings.stream()
-                .filter(c->c.getKey().equals(key)).findFirst();
+                .filter(c -> c.getKey().equals(key)).findFirst();
         if (con != null) return con.get().getValue();
         return defaultVal;
     }
